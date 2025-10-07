9a8c6c5c01d34634f4921053e2c45a5128989048
tavo <tavo@tavo.one>
2025-10-07 00:57:44 -0600

    versions

--- gfd/i18n/en.py
+++ gfd/i18n/en.py
@@ -1,4 +1,7 @@
 translations = {
     "hello_world": "Hello, World!",
     "window_title": "Digital Signature Manager",
+    "no_installers_found": "Could not update installer list",
+    "loading_installers": "Loading Installers",
+    "refresh": "Refresh",
 }
--- gfd/i18n/es.py
+++ gfd/i18n/es.py
@@ -1,4 +1,7 @@
 translations = {
     "hello_world": "¡Hola, Mundo!",
     "window_title": "Gestor de Firma Digital",
+    "no_installers_found": "No se pudo actualizar la lista de instaladores",
+    "loading_installers": "Cargando instaladores",
+    "refresh": "Buscar",
 }
